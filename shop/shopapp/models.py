3b2fe458eda24c19d4d14e88e511b07ae60f5d6d
SagarR.B <sagarrb1805@gmail.com>
2022-08-25 15:35:06 +0530

    django and strip

--- shop/shopapp/models.py
+++ shop/shopapp/models.py
@@ -1,3 +1,24 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=5, decimal_places=2)
+    status = models.BooleanField(default=True)
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=100)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField(max_length=100)
+
+class CustomOrder(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField(default=100)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField(max_length=100)
+    order_details = models.TextField()
--- shop/shopapp/views.py
+++ shop/shopapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . models import Product, Order, CustomOrder
 # Create your views here.
+
+
+
+# admin : 
+# username : admin
+# password : 1234
+
+
+def index(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    if request.method == 'POST' and 'purchase' in request.POST:
+        product_id_pur = request.POST.get('product_id_pur')
+        if product_id_pur != 'Others':
+            product = Product.objects.get(product_id = product_id_pur)
+            customer_name = request.POST.get("customer_name")
+            customer_email = request.POST.get('customer_email')
+            quantity = request.POST.get('quantity')
+            Order.objects.create(product_id=product, quantity=quantity, customer_name=customer_name, customer_email=customer_email)
+            return redirect('/')
+        else:
+            customer_name = request.POST.get("customer_name")
+            customer_email = request.POST.get('customer_email')
+            quantity = request.POST.get('quantity')
+            order_details = request.POST.get('custom_order')
+            CustomOrder.objects.create(quantity=quantity, customer_name=customer_name, customer_email=customer_email, order_details=order_details)
+            return redirect('/')
+    if request.method == 'POST' and 'check' in request.POST:
+        
+        product_id_pur = request.POST.get('product_id_pur')
+        if product_id_pur != 'Others':
+            product = Product.objects.get(product_id = product_id_pur)
+            context = {'products': products, 'description':product.product_description, 'price':product.product_price}
+        
+    return render(request, 'index.html', context)
